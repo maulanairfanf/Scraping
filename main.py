f38613c1c1b143f2dcb943582a7e15718dded29b
maulanairfanf <maulanairfanf@gmail.com>
2022-04-18 23:50:58 +0700

    looping dataframe

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from sqlalchemy import create_engine
+from turtle import title
+from colorama import Cursor
+import psycopg2.extras as extras
+import psycopg2
 import pandas as pd
+from sqlalchemy import create_engine
 from liputan import listLiputan
-from tribun import listTribun
-from detik import listDetik
+# from tribun import listTribun
+# from detik import listDetik
 from help import currentDateTime
 
 # print(listLiputan)
@@ -13,17 +17,59 @@ from help import currentDateTime
 # listBerita.extend(listDetik) #buat JSON
 # listBerita.extend(listTribun)
 
-arrBerita = [listLiputan,listTribun,listDetik]
-listBerita = pd.concat(arrBerita)
-listBerita.reset_index(drop=True, inplace=True)
-listBerita.drop_duplicates(subset=["link"])
-listBerita.to_csv(f"Data/Berita{currentDateTime}.csv",index=False)
+# arrBerita = [listLiputan, listDetik]
+# listLiputan = pd.concat(arrBerita)
+listLiputan.reset_index(drop=True, inplace=True)
+listLiputan.drop_duplicates(subset=["link"])
+# listLiputan.to_csv(f"Data/Berita({currentDateTime}).csv", index=False)
 
-engine = create_engine('postgresql://sgipiulsrqjqyd:c03109765d8a9574c3adc9273c6e07f531997fbfe0722e1c40e93d8184b0b53f@ec2-34-231-63-30.compute-1.amazonaws.com:5432/durspbi4nbo1m') 
+engine = create_engine(
+    'postgresql://postgres:TanpaPassword@localhost:5432/webScraping')
+# engine = create_engine(
+#     "postgresql://vrsvsluhxmvrnh:1be4aa85913309c31a26df4842d9763f7dd4dca289aad373fea555a3542d499f@ec2-34-192-210-139.compute-1.amazonaws.com:5432/deqif9hrnf9p2t")
+# conn = psycopg2.connect()
+conn = psycopg2.connect(
+    database="webScraping", user='postgres', password='TanpaPassword', host='127.0.0.1', port='5432'
+)
 
-listBerita.to_sql('berita',con=engine,if_exists='append',index=False) 
+cursor = conn.cursor()
 
-# print(engine.execute("SELECT * FROM berita").fetchnone())
 
-# with open("Berita.json",'w') as f :
-#     f.write(json.dumps(listBerita))
+for index, row in listLiputan.iterrows():
+    title = row["title"]
+    date = row["date"]
+    author = row["author"]
+    link = row["link"]
+    category = row["category"]
+    website = row["website"]
+    content = row["content"]
+    query = """INSERT into berita(title,date,author,link,category,website,content) VALUES (%s,%s,%s,%s,%s,%s,%s);"""
+    column = (title, date, author, link, category, website, content)
+    cursor.execute(query, column)
+    conn.commit()
+
+# def execute_values(conn, df, table):
+
+#     tuples = [tuple(x) for x in df.to_numpy()]
+
+#     cols = ','.join(list(df.columns))
+#     print(cols)
+#     # SQL query to execute
+#     query = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
+#     cursor = conn.cursor()
+#     try:
+#         extras.execute_values(cursor, query, tuples)
+#         conn.commit()
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print("Error: %s" % error)
+#         conn.rollback()
+#         cursor.close()
+#         return 1
+#     print("the dataframe is inserted")
+#     cursor.close()
+
+
+# execute_values(conn, listLiputan, 'berita')
+
+
+# listLiputan.to_sql('berita', con=conn, if_exists='append', index=False)
