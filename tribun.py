bb3e71c038d44aa0879b9219b419d0e59e2fddc1
maulanairfanf <maulanairfanf@gmail.com>
2022-04-25 16:00:21 +0700

    kerangka

--- tribun.py
+++ tribun.py
@@ -82,7 +82,7 @@ def setUp(new, category):
     link_new = new['href']
     html_link_new = requests.get(link_new, headers=headers).text
     soup_new = BeautifulSoup(html_link_new, 'lxml')
-    rules(soup_new, link_new, category)
+    kerangkaTribun(soup_new, link_new, category)
 
 
 items = []
